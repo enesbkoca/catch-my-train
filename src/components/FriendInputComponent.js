9c557bc986cb285ca7239a4bd0c6cfc51cfc9805
enesbkoca <enesbkoca@gmail.com>
2024-10-06 18:56:15 +0200

    Add new meeting options and simplify the design

--- src/components/FriendInputComponent.js
+++ src/components/FriendInputComponent.js
@@ -2,10 +2,28 @@ import React, { useEffect, useState } from 'react';
 import GetStations from "../functions/StationList.js";
 
 const FriendInputComponent = () => {
-    const [friends, setFriends] = useState([{
-        name: '', station: '', earliest_time: '', latest_time: '', duration: ''
-    }]);
+    const [friends, setFriends] = useState([
+            {name: '', station: ''}, {name: '', station: ''}
+        ]
+    );
     const [stationOptions, setStationOptions] = useState([]);
+
+    const getTodaysDate = () => {
+        const now = new Date();
+        return String(now.getDate()).padStart(2, '0');
+    }
+
+    const getOneHourAheadTime = () => {
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+
+        return `${hours}:${minutes}`; // Format to HH:MM
+    };
+
+    const [meetingOptions, setMeetingOptions] = useState([{
+        date: getTodaysDate(), meeting_time: getOneHourAheadTime(), duration: '03:00'}])
 
     useEffect(() => {
         const fetchStations = async () => {
@@ -24,17 +42,15 @@ const FriendInputComponent = () => {
     const inputConfig = [
         { type: 'text', placeholder: 'Name', field: 'name' },
         { type: 'select', placeholder: 'Starting Station', field: 'station', options: stationOptions},
-        { type: 'time', placeholder: 'Earliest Departure Time', field: 'earliest_time' },
-        { type: 'time', placeholder: 'Latest Departure Time', field: 'latest_time' },
-        { type: 'time', placeholder: 'Meetup Duration', field: 'duration' },
-
     ];
+
+
 
     // func to handle adding new rows
     const addFriend = () => {
         if (friends.length < 5) {
             setFriends([...friends, {
-                name: '', station: '', earliest_time: '', latest_time: '', duration: ''
+                name: '', station: '',
             }]);
         }
     };
@@ -52,10 +68,16 @@ const FriendInputComponent = () => {
         setFriends(newFriends);
     };
 
+    const handleMeetingChange = (field, value) => {
+        const newOptions = [...meetingOptions];
+        newOptions[field] = value;
+        setMeetingOptions(newOptions);
+    }
+
     return (
         <div className="user-inputs">
             {friends.map((friend, index) => (
-                <div key={index} className="friend-row">
+                <div key={index} className="row friend">
                     {/* Dynamically generate inputs */}
                     {inputConfig.map((input, inputIndex) => (
                         input.type === 'select' ? (
@@ -64,7 +86,7 @@ const FriendInputComponent = () => {
                                 value={friend[input.field]}
                                 onChange={(e) => handleInputChange(index, input.field, e.target.value)}
                             >
-                                <option value="">Select {input.placeholder}</option>
+                                <option value="">{input.placeholder}</option>
                                 {input.options.map((option, optIndex) => (
                                     <option key={optIndex} value={option}>
                                         {option}
@@ -82,17 +104,37 @@ const FriendInputComponent = () => {
                         )
                     ))}
 
-                    {friends.length > 1 && (
-                        <button className="remove-button" onClick={() => removeFriend(index)}>
-                            &ndash;
-                        </button>
-                    )}
+                    <button className="remove-button" onClick={() => removeFriend(index)} disabled={friends.length < 3}>
+                        &ndash;
+                    </button>
                 </div>
             ))}
 
-            <button onClick={addFriend} disabled={friends.length >= 5}>
-                Add Friend
+            <div className="row meeting-options">
+                <input
+                    type="date"
+                    value={meetingOptions.date}
+                    onChange={(e) => handleMeetingChange(meetingOptions["date"], e.target.value)}
+                />
+                <input
+                    type="time"
+                    value={meetingOptions.meeting_time}
+                    onChange={(e) => handleMeetingChange(meetingOptions["time"], e.target.value)}
+                />
+                <input
+                    type="time"
+                    value={meetingOptions.duration}
+                    onChange={(e) => handleMeetingChange(meetingOptions["duration"], e.target.value)}
+                />
+                <button className="add-button" onClick={addFriend} disabled={friends.length >= 5}>
+                    +
+                </button>
+            </div>
+
+            <button className="submit">
+                Find the perfect train
             </button>
+
         </div>
     );
 };
