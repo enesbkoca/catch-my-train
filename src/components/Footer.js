c0bca61b31e4736f3f076771bfd2c599c6c1a958
enesbkoca <enesbkoca@gmail.com>
2024-10-13 10:06:50 +0200

    Removed zoom button, and beautified the footer and header

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="tagline">Helping people catch their trains one journey at a time! 🚄</p>
+            <p className="copyright">© 2024 Catch My Train. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
