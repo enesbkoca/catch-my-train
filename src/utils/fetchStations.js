3257d2df8f29516d2a2145d3914864a38657547e
enesbkoca <enesbkoca@gmail.com>
2024-10-13 09:05:18 +0200

    Re-organize folder structure

--- src/utils/fetchStations.js
+++ src/utils/fetchStations.js
@@ -1,7 +1,7 @@
 import Papa from 'papaparse';
 
 const GetStations = async () => {
-    const filePath = process.env.PUBLIC_URL + '/stations-2023-09.csv';
+    const filePath = process.env.PUBLIC_URL + '/data/stations-2023-09.csv';
 
     const response = await fetch(filePath);
     const csvData = await response.text();
@@ -12,14 +12,12 @@ const GetStations = async () => {
         skipEmptyLines: true
     });
 
-    let stations = parsedData.data
+    return parsedData.data
         .filter(station => station.country === 'NL')
         .map(station => ({
             name: station.name_long.replace(/"/g, ''),
             coordinates: [parseFloat(station.geo_lat), parseFloat(station.geo_lng)]
         }));
-
-    return stations;
 }
 
 export default GetStations;
