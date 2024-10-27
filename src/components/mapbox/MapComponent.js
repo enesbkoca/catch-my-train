b58cdaee9a2178b00b04cdc929b60b27c82c400d
enesbkoca <enesbkoca@gmail.com>
2024-10-27 08:36:04 +0100

    Make polylines more curved and dashed

--- src/components/mapbox/MapComponent.js
+++ src/components/mapbox/MapComponent.js
@@ -1,33 +1,68 @@
-import React, { useContext } from 'react';
-import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
+import React, { useContext, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import { MapContext } from './MapContext';
+
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import {coordinatesNetherlands} from "../../assets/constants";
+import 'leaflet-curve';
+import 'leaflet-polylinedecorator';
+
+
 import {createCustomIcon} from "../../utils/helperFunctions";
+import {coordinatesNetherlands} from "../../assets/constants";
+
+const CurvedPolyline = ({ positions, color }) => {
+    const map = useMap();
+
+    useEffect(() => {
+        if (positions.length < 2) return;
+
+        const pathCommands = [];
+        for (let i = 0; i < positions.length - 1; i++) {
+            const start = positions[i];
+            const end = positions[i + 1];
+            const controlPoint = [
+                (start[0] + end[0]) / 2 + 0.05,
+                (start[1] + end[1]) / 2 - 0.05,
+            ];
+
+            pathCommands.push('M', start, 'Q', controlPoint, end);
+        }
+
+        const curve = L.curve(pathCommands, { color: color || 'blue', weight: 4, dashArray: '5, 10' });
+        curve.addTo(map);
+
+        return () => map.removeLayer(curve);
+    }, [map, positions, color]);
+
+    return null;
+};
 
 const MapComponent = () => {
     const { markers, polylines } = useContext(MapContext);
 
     return (
-        <MapContainer center={coordinatesNetherlands} zoom={8} className={"map-container"} zoomControl={false}>
+        <MapContainer center={coordinatesNetherlands} zoom={8} className="map-container" zoomControl={false}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+
             {markers.map((marker, index) => (
                 <Marker
-                    key={index}
+                    key={`marker-${index}`}
                     position={marker.position}
                     icon={createCustomIcon(marker.color)}
-                ></Marker>
+                >
+                </Marker>
             ))}
 
+            {/* Render curved polylines with dash pattern */}
             {polylines.map((polyline, index) => (
-                <Polyline
-                    key={`polyline-${index}`}
+                <CurvedPolyline
+                    key={`curved-polyline-${index}`}
                     positions={polyline.positions}
-                    color={polyline.color || 'blue'}
-                    weight={4}
+                    color={polyline.color}
                 />
             ))}
         </MapContainer>
