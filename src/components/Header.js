53190f2cb4bdc9d8ab2636b19eefb35be8f97d96
enesbkoca <enesbkoca@gmail.com>
2024-10-28 09:07:58 +0100

    Add new logos to favicon and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,14 @@ const Header = () => {
     return (
         <header className="header">
             {/*I will help you catch your train :)*/}
+            <img src={`${process.env.PUBLIC_URL}/assets/logo_full_white.png`} alt="Logo" className="header-logo"/>
             <nav>
                 <ul className="nav-list">
                     <li><Link className="nav-link" to="/">Home</Link></li>
                     <li><Link className="nav-link" to="/planner">Planner</Link></li>
                     <li><Link className="nav-link" to="/journey">Journey</Link></li>
-                    <li><a className="nav-link" href="https://github.com/enesbkoca/catch-my-train" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                    <li><a className="nav-link" href="https://github.com/enesbkoca/catch-my-train" target="_blank"
+                           rel="noopener noreferrer">GitHub</a></li>
                 </ul>
             </nav>
         </header>
