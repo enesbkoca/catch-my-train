f8e24f052acace007176088e8b5656c09d197966
enesbkoca <enesbkoca@gmail.com>
2025-01-12 14:15:53 +0100

    Fix re-render due to markers dependency in useEffect

--- src/components/planner/PlannerInput.js
+++ src/components/planner/PlannerInput.js
@@ -24,7 +24,7 @@ const PlannerInput = ({ onSubmit }) => {
 
     useEffect(() => {
         updateFriendInputMarkers(friends, stations, markers, addMarker, removeMarker, updateMarker);
-    }, [friends, stations, markers, addMarker, removeMarker, updateMarker]);
+    }, [friends, stations]);
 
     useEffect(() => {
         console.log("Markers have been updated: ", markers);
